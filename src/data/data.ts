99b93ed03b45cfbeeb62ca86d0b260f372021769
danibystrom <124156950+danibystrom@users.noreply.github.com>
2024-08-12 14:52:17 +0200

    added data.ts for future-files

---
+++ src/data/data.ts
@@ -0,0 +1,103 @@
+export interface Photo {
+  id: number;
+  url: string;
+  category: string;
+}
+
+export const photos: Photo[] = [
+  {
+    id: 1,
+    url: "image1.jpg",
+    category: "Nature",
+  },
+  {
+    id: 2,
+    url: "image2.jpg",
+    category: "City",
+  },
+  {
+    id: 3,
+    url: "image3.jpg",
+    category: "Nature",
+  },
+  {
+    id: 4,
+    url: "image4.jpg",
+    category: "City",
+  },
+  {
+    id: 5,
+    url: "image1.jpg",
+    category: "Nature",
+  },
+  {
+    id: 6,
+    url: "image2.jpg",
+    category: "City",
+  },
+  {
+    id: 7,
+    url: "image3.jpg",
+    category: "Nature",
+  },
+  {
+    id: 8,
+    url: "image4.jpg",
+    category: "City",
+  },
+  {
+    id: 9,
+    url: "image1.jpg",
+    category: "Nature",
+  },
+  {
+    id: 10,
+    url: "image2.jpg",
+    category: "City",
+  },
+  {
+    id: 11,
+    url: "image3.jpg",
+    category: "Nature",
+  },
+  {
+    id: 12,
+    url: "image4.jpg",
+    category: "City",
+  },
+  {
+    id: 13,
+    url: "image1.jpg",
+    category: "Nature",
+  },
+  {
+    id: 14,
+    url: "image2.jpg",
+    category: "City",
+  },
+  {
+    id: 15,
+    url: "image3.jpg",
+    category: "Nature",
+  },
+  {
+    id: 16,
+    url: "image4.jpg",
+    category: "City",
+  },
+  {
+    id: 17,
+    url: "image1.jpg",
+    category: "Nature",
+  },
+  {
+    id: 18,
+    url: "image2.jpg",
+    category: "City",
+  },
+  {
+    id: 19,
+    url: "image3.jpg",
+    category: "Nature",
+  },
+];
